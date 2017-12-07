72aa7b9be58c3b96a268c03a6ec50d6ab219eb22
H. Lockwood <hanford95@gmail.com>
2017-12-07 12:51:19 -0500

    Basic Additions
    Added New Diction Matrix

--- AntiquatedMessagingSystem.py
+++ AntiquatedMessagingSystem.py
@@ -43,7 +43,7 @@ class Cypher(inMessage, language):
 	def send():
 
 	def diction():
-		diction = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z]	
+		diction = diction = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 				  []
 
 #symbols
