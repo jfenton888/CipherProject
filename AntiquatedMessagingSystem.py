23063ace661bee5cc7a3c4a1f34cbee0cfbb5177
H. Lockwood <hanford95@gmail.com>
2017-12-07 12:05:46 -0500

    Basic

--- AntiquatedMessagingSystem.py
+++ AntiquatedMessagingSystem.py
@@ -22,7 +22,29 @@ class MorseCode(inMessage, language):
 
 
 #letters changed
+class Cypher(inMessage, language):
+	"""docstring for MorseCode"""
 
+
+	def __init__(self, arg):
+		self.transTo = transTo
+		self.transBack = transBack
+		self.send = send
+		self.diction = diction
+
+
+	def transTo():
+		msg = str(input('Please input the text you would like to have translated'))
+
+
+
+	def transBack():
+
+	def send():
+
+	def diction():
+		diction = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z]	
+				  []
 
 #symbols
 
