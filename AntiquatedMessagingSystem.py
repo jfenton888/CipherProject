dbbad68e71b7ba8196045de0f1d58b09ba8b0f76
Revant Kantamneni <rkantamneni20@choate.edu>
2017-12-07 11:52:28 -0500

    Added user interface

--- AntiquatedMessagingSystem.py
+++ AntiquatedMessagingSystem.py
@@ -1,4 +1,7 @@
 #morse code
+#letter change/cipher
+#symbols
+
 class MorseCode(inMessage, language):
 	"""docstring for MorseCode"""
 	def __init__(self, arg):
@@ -12,10 +15,22 @@ class MorseCode(inMessage, language):
 
 	def send():
 
+
+
 	
-
-
-
+work = True
+while work==True:
+	response1 = sys.arg("Would you like to translate to[1] or translate back[2]? ")
+	if response1 = "1":
+		response11 = sys.arg("What language would you like translate in to? [1]Morse [2]Cipher [3] Symbols")
+			if response11 = '1':
+				MorseCode.transTo()
+			if response11 = '2':
+				CipherCode.transTo()
+			if response11 = '3':
+				SymbolsCode.tranTo()
+	elif response1 ="2":
+	response2 = sys.arg("What language ")
 #letters changed
 
 
