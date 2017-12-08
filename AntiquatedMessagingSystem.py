6c1c3a40ff414b5a1f7b88a2e0ba9ffeba5ee2b5
Revant Kantamneni <rkantamneni20@choate.edu>
2017-12-08 13:01:13 -0500

    2

--- AntiquatedMessagingSystem.py
+++ AntiquatedMessagingSystem.py
@@ -29,19 +29,7 @@ class Cipher(inMessage, inLanguage):
 
 
 	
-work = True
-while work==True:
-	response1 = sys.arg("Would you like to translate to[1] or translate back[2]? ")
-	if response1 = "1":
-		response11 = sys.arg("What language would you like translate in to? [1]Morse [2]Cipher [3] Symbols")
-			if response11 = '1':
-				MorseCode.transTo()
-			if response11 = '2':
-				CipherCode.transTo()
-			if response11 = '3':
-				SymbolsCode.tranTo()
-	elif response1 ="2":
-	response2 = sys.arg("What language ")
+
 #letters changed
 
 
