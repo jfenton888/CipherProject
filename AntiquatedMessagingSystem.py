793ee664673472104acf061b3fbd84962680ba85
jfenton888 <mllax8@me.com>
2017-12-07 19:12:48 -0500

    aa

--- AntiquatedMessagingSystem.py
+++ AntiquatedMessagingSystem.py
@@ -1,27 +1,31 @@
-#morse code
-class MorseCode(inMessage, inLanguage):
-	"""docstring for MorseCode"""
+class Cipher(inMessage, inLanguage):
+	
+	def __init__(self, chooseLanguage):
+		slef.chooseLanguage = inLanguage
+
+	class MorseCode(inMessage):
+		"""docstring for MorseCode"""
 
 
-	def __init__(self, arg):
-		self.transTo = transTo
-		self.transBack = transBack
-		self.send = send
-		self.diction = diction
+		def __init__(self, arg):
+			self.transTo = transTo
+			self.transBack = transBack
+			self.send = send
+			self.diction = diction
 
 
-	def transTo():
-		letter1= letter
-	def transBack():
+		def transTo():
+			letter1= letter
+		def transBack():
 
-	def send():
+		def send():
 
-	def diction():
-		diction = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'][w,r,t]
-
-
-#letters changed
+		def diction():
+			diction = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'][w,r,t]
 
 
-#symbols
+	#letters changed
+
+
+	#symbols
 
