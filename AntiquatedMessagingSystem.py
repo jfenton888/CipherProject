5393a6a913fb13582f82f6754827c347161153b3
Revant Kantamneni <rkantamneni20@choate.edu>
2017-12-08 12:56:28 -0500

    new

--- AntiquatedMessagingSystem.py
+++ AntiquatedMessagingSystem.py
@@ -1,19 +1,30 @@
 #morse code
 #letter change/cipher
 #symbols
+class Cipher(inMessage, inLanguage):
+	
+	def __init__(self, chooseLanguage):
+		slef.chooseLanguage = inLanguage
 
-class MorseCode(inMessage, language):
-	"""docstring for MorseCode"""
-	def __init__(self, arg):
-		self.transTo = transTo
-		self.transBack = transBack
-		self.send = send
+	class MorseCode(inMessage):
+		"""docstring for MorseCode"""
 
-	def transTo():
 
-	def transBack():
+		def __init__(self, arg):
+			self.transTo = transTo
+			self.transBack = transBack
+			self.send = send
+			self.diction = diction
 
-	def send():
+
+		def transTo():
+			letter1= letter
+		def transBack():
+
+		def send():
+
+		def diction():
+			diction = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'][w,r,t]
 
 
 
