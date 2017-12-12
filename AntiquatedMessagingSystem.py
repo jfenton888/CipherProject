f2727ac74c7056c6ccbd298c5c5a29ff1029e498
Revant Kantamneni <rkantamneni20@choate.edu>
2017-12-11 20:45:54 -0500

    Added classes

--- AntiquatedMessagingSystem.py
+++ AntiquatedMessagingSystem.py
@@ -1,10 +1,7 @@
-#morse code
-#letter change/cipher
-#symbols
 class Cipher(inMessage, inLanguage):
 	
 	def __init__(self, chooseLanguage):
-		slef.chooseLanguage = inLanguage
+		self.chooseLanguage = inLanguage
 
 	class MorseCode(inMessage):
 		"""docstring for MorseCode"""
@@ -16,22 +13,82 @@ class Cipher(inMessage, inLanguage):
 			self.send = send
 			self.diction = diction
 
+		def transTo():
+			for i in range(len(inMessage)):
+				outMessage[i] = self.diction(i)
+
+		def transBack():
+			for b in range(len(inMessage)):
+				outMessage[b] = self.diction(b)
+
+		def diction(i):
+			corespond = [[' abcdefghijklmnopqrstuvwxyz0123456789'],[' ', '•– ', '–••• ', '–•–• ', '–•• ', '• ', '••–• ', '––• ', '•••• ', '•• ', '•––– ', '–•– ', '•–•• ', '–– ', '–• ', '––– ', '•––• ', '––•– ', '•–• ', '••• ', '– ', '••– ', '•••– ', '•–– ', '–••– ', '–•–– ', '––•• ', '––––– ', '•–––– ', '••––– ', '•••–– ', '••••– ', '••••• ', '–•••• ', '––••• ', '–––•• ', '––––• ', '']]
+			return corespond[1][corespond[0][0].find(word[i])]
+
+		def diction2(b):
+			corespond2 = [[' ', '•– ', '–••• ', '–•–• ', '–•• ', '• ', '••–• ', '––• ', '•••• ', '•• ', '•––– ', '–•– ', '•–•• ', '–– ', '–• ', '––– ', '•––• ', '––•– ', '•–• ', '••• ', '– ', '••– ', '•••– ', '•–– ', '–••– ', '–•–– ', '––•• ', '––––– ', '•–––– ', '••––– ', '•••–– ', '••••– ', '••••• ', '–•••• ', '––••• ', '–––•• ', '––––• ', ''], [' abcdefghijklmnopqrstuvwxyz0123456789']]
+			return corespond2[1][corespond2[0][0].find(word[b])]
+
+	class LetterNum(inMessage):
+		"""docstring for MorseCode"""
+
+		def __init__(self, arg):
+			self.transTo = transTo
+			self.transBack = transBack
+			self.send = send
+			self.diction = diction
 
 		def transTo():
-			letter1= letter
+			for i in range(len(inMessage)):
+				outMessage[i] = self.diction(i)
+
 		def transBack():
+			for b in range(len(inMessage)):
+				outMessage[b] = self.diction(b)
 
-		def send():
+		def diction(i):
+			corespond = [['abcdefghijklmnopqrstuvwxyz0123456789'],['1','2','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31','32','33','34','35','36']]
+			return corespond[1][corespond[0][0].find(word[i])]
 
-		def diction():
-			diction = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'][w,r,t]
-
-
-
-	
-
-#letters changed
+		def diction2(b):
+			corespond2 = [['1','2','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31','32','33','34','35','36'], ['abcdefghijklmnopqrstuvwxyz0123456789']]
+			return corespond2[1][corespond2[0][0].find(word[b])]
 
 
-#symbols
+	class LetterShift(inMessage):
+		"""docstring for MorseCode"""
+
+
+		def __init__(self, arg):
+			self.transTo = transTo
+			self.transBack = transBack
+			self.send = send
+			self.diction = diction
+
+		def transTo():
+			for i in range(len(inMessage)):
+				outMessage[i] = self.diction(i)
+
+		def transBack():
+			for b in range(len(inMessage)):
+				outMessage[b] = self.diction(b)
+
+		def diction(i):
+			corespond = [['abcdefghijklmnopqrstuvwxyz0123456789'],['BCDEFGHIJKLMNOPQRSTUVWXYZ0123456789A']]
+			return corespond[1][corespond[0][0].find(word[i])]
+
+		def diction2(b):
+			corespond2 = [['BCDEFGHIJKLMNOPQRSTUVWXYZ0123456789A'],['abcdefghijklmnopqrstuvwxyz0123456789']]
+			return corespond2[1][corespond2[0][0].find(word[b])]
+
+
+
+'''
+cipher = Cipher()
+morse = Cipher.MorseCode
+morse.
+	#letters changed
+
+'''
+	#symbols
 
