96116c33c356275ddfb55d4904bd2192d34f3e20
jfenton888 <mllax8@me.com>
2017-12-12 00:20:08 -0500

    Write to…

--- AntiquatedMessagingSystem.py
+++ AntiquatedMessagingSystem.py
@@ -1,11 +1,14 @@
-# class Cipher(inMessage, inLanguage):
-	
-# 	def __init__(self, chooseLanguage):
-# 		self.chooseLanguage = inLanguage
+
+
+
+def sendToFile(encodedMessage):
+	f = open('helloworld.txt','a')
+	f.write(encodedMessage)
+	f.close()
+
+	print('Message Sent!')
 
 class MorseCode:
-	"""docstring for MorseCode"""
-
 	def __init__(self):
 		self.diction = [[' abcdefghijklmnopqrstuvwxyz0123456789'],[' ', '•– ', '–••• ', '–•–• ', '–•• ', '• ', '••–• ', '––• ', '•••• ', '•• ', '•––– ', '–•– ', '•–•• ', '–– ', '–• ', '––– ', '•––• ', '––•– ', '•–• ', '••• ', '– ', '••– ', '•••– ', '•–– ', '–••– ', '–•–– ', '––•• ', '––––– ', '•–––– ', '••––– ', '•••–– ', '••••– ', '••••• ', '–•••• ', '––••• ', '–––•• ', '––––• ', '']]
 		self.outMessage = []
@@ -13,34 +16,69 @@ class MorseCode:
 	def transTo(self, inMessage):
 
 		for i in range(len(inMessage)):
-			# print(i)
-			# print(len(inMessage))
-			# print(inMessage[i])
-			#print(self.diction[0][0].find(inMessage[i]))
-			# print(self.diction[1][self.diction[0][0].find(inMessage[i])])
-			# print(self.diction[1][0])
 			code = (self.diction[1][self.diction[0][0].find(inMessage[i])])
 			(self.outMessage).append(code)
 		return MorseCode.send()
-
 
 	def send(self):
 		message = ''.join(self.outMessage)
 		return message
 
-	# def diction(self):
+class LetterNum:
+		def __init__(self):
+		self.diction = [[' abcdefghijklmnopqrstuvwxyz0123456789'],['1','2','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31','32','33','34','35','36']]
+		self.outMessage = []
 
-		
-	# 	corespond = [[' abcdefghijklmnopqrstuvwxyz0123456789'],[' ', '•– ', '–••• ', '–•–• ', '–•• ', '• ', '••–• ', '––• ', '•••• ', '•• ', '•––– ', '–•– ', '•–•• ', '–– ', '–• ', '––– ', '•––• ', '––•– ', '•–• ', '••• ', '– ', '••– ', '•••– ', '•–– ', '–••– ', '–•–– ', '––•• ', '––––– ', '•–––– ', '••––– ', '•••–– ', '••••– ', '••••• ', '–•••• ', '––••• ', '–––•• ', '––––• ', '']]
-	# 	return 
+	def transTo(self, inMessage):
 
-#letters changed
+		for i in range(len(inMessage)):
+			code = (self.diction[1][self.diction[0][0].find(inMessage[i])])
+			(self.outMessage).append(code)
+		return MorseCode.send()
+
+	def send(self):
+		message = ''.join(self.outMessage)
+		return message
 
 
-#symbols
+class LetterShift:
+		def __init__(self):
+		self.diction = [[' abcdefghijklmnopqrstuvwxyz0123456789'],['BCDEFGHIJKLMNOPQRSTUVWXYZ0123456789A']]
+		self.outMessage = []
+
+	def transTo(self, inMessage):
+
+		for i in range(len(inMessage)):
+			code = (self.diction[1][self.diction[0][0].find(inMessage[i])])
+			(self.outMessage).append(code)
+		return MorseCode.send()
+
+	def send(self):
+		message = ''.join(self.outMessage)
+		return message
+
 
 MorseCode = MorseCode()
+LetterNum = LetterNum()
+LetterShift = LetterShift()
+language = '0' 
+
+while (('morse' not in language) and ('l2n' not in language) and ('ls' not in language) and ('sc' not in language)):
+	language = (input('What code language do you want your message changed into? \n Morse Code(Morse), a letter to number translation(L2N), a letter shift(LS), or a symbol cipher(SC)?')).lower
+if 'morse' in language:
+	messIn = (input('what should become MorseCode? ')).lower
+	MorseCode.transTo(messIn))
+elif 'l2n' in language:
+	messIn = (input('what should become go into the letter shift? ')).lower
+	LetterNum.transTo(messIn))
+elif 'ls' in language:
+	messIn = (input('what should become numbers? ')).lower 
+	LetterShift.transTo(messIn))
+elif 'sc' in language:
+	messIn = (input('what should become a symbol cipher? ')).lower
+	SymbolCiph.transTo(messIn))
 
 
-messIn = input('what should become MorseCode? ')
-print(MorseCode.transTo(messIn))
+
+
+
